9a8d56ce487282e60be017177e99d4a58336f487
peach <kittisophon.su872@gmail.com>
2025-08-01 09:40:20 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/Header'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import CardDetail from './pages/CardDetail'
+import NotFound from './pages/NotFound'
+
+const App = () => {
+  return (
+    <>
+      <Router>
+
+    
+        <div className='flex flex-col min-h-screen'>
+          <Header/>
+
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/collection' element={<Collection/>} />
+            <Route path='/About' element={<About/>} />
+            <Route path='/card_detail/:id'element={<CardDetail/>} />
+            <Route path='*' element={<NotFound/>} />
+
+          </Routes>
+        </div>
+      </Router>
+      
+    </>
+  )
+}
+
+export default App
